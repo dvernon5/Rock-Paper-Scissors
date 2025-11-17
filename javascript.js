b41d9a35d0cbd8127c0d6fba31fb7cd5d86fd329
Donovan Vernon <d.vernon5@gmail.com>
2025-11-17 13:22:04 -0800

    Switch let to const in getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let choice = ['rock', 'paper', 'scissors'];
+    const choice = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * 3);
 
     return choice[randomIndex];
